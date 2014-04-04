a5ea8aaea241214610a928af43aa744743385b89
alumno <alumno@localhost>
2014-04-04 11:11:23 -0300

    primer commit

---
+++ taller/src/negro/datosolo.java
@@ -0,0 +1,13 @@
+package negro;
+
+public class datosolo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("primer commit");
+	}
+
+}
